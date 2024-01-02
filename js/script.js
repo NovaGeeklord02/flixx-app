1e25692e7f8bb43df395b21c27c06770f862d358
Ankit Ekka <97518128+NovaGeeklord02@users.noreply.github.com>
2024-01-02 20:56:48 +0530

    added movie details

--- js/script.js
+++ js/script.js
@@ -66,6 +66,58 @@ async function DisplayPopTV(){
     })
 }
 
+async function DisplayMovieDetails(){
+    const movieID = await document.location.search.split('=')[1];
+    
+    const movie = await FetchfromDMTB(`movie/${movieID}`)
+    console.log(movie);
+    const div = document.createElement('div')
+    div.innerHTML = `<div class="details-top">
+    <div>
+      <img
+        src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+        class="card-img-top"
+        alt="${movie.title}"
+      />
+    </div>
+    <div>
+      <h2>${movie.title}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${movie.vote_average.toFixed(1)}
+      </p>
+      <p class="text-muted">Release Date: ${movie.release_date}</p>
+      <p>
+        ${movie.overview}
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+      ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+      </ul>
+      <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+    </div>
+  </div>
+  <div class="details-bottom">
+    <h2>Movie Info</h2>
+    <ul>
+      <li><span class="text-secondary">Budget:</span> ${movie.budget}</li>
+      <li><span class="text-secondary">Revenue:</span> ${movie.revenue}</li>
+      <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+      <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">
+       ${movie.production_companies
+         .map((comp) => `<span>${comp.name}</span>`).join(', ')
+       }
+    </div>
+  </div>
+    `
+    document.querySelector('#movie-details').appendChild(div);
+}
+
+
+
 async function FetchfromDMTB(endpoint){
     API_KEY = '26dfc1054fa8edb91389180f69e91e58'
     API_URL = 'https://api.themoviedb.org/3/'
@@ -103,7 +155,7 @@ function init(){
             console.log('TV Details');
             break;
         case '/movie-details.html':
-            console.log('Movie Details');
+            DisplayMovieDetails();
             break;
         case '/search.html':
             console.log('Search Details'); 
