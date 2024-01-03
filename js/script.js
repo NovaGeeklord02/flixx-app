14020cb2f71086411700747dba986b2668bdcaa5
Ankit Ekka <97518128+NovaGeeklord02@users.noreply.github.com>
2024-01-03 17:22:22 +0530

    search results

--- js/script.js
+++ js/script.js
@@ -249,7 +249,52 @@ async function search(){
   // console.log(global.search.term);
   const data = await fetchSearchData();
   const dataArray = await data.results;
-  console.log(data);
+  displaySearchResults(dataArray);
+}
+
+
+
+
+function displaySearchResults(results) {
+  results.forEach((result) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="${global.search.type}-details.html?id=${result.id}">
+            ${
+              result.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500/${result.poster_path}"
+              class="card-img-top"
+              alt="${
+                global.search.type === 'movie' ? result.title : result.name
+              }"
+            />`
+                : `<img
+            src="../images/no-image.jpg"
+            class="card-img-top"
+             alt="${
+               global.search.type === 'movie' ? result.title : result.name
+             }"
+          />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${
+              global.search.type === 'movie' ? result.title : result.name
+            }</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${
+                global.search.type === 'movie'
+                  ? result.release_date
+                  : result.first_air_date
+              }</small>
+            </p>
+          </div>
+        `;
+
+    document.querySelector('#search-results').appendChild(div);
+  });
 }
 
 
