8a69cfd789604610b2c725846f99323634693afb
Ankit Ekka <97518128+NovaGeeklord02@users.noreply.github.com>
2024-01-03 12:24:59 +0530

    added slider

--- js/script.js
+++ js/script.js
@@ -167,6 +167,50 @@ async function DisplayTVShowDetails() {
   document.querySelector('#show-details').appendChild(div);
 }
 
+async function DisplaySlider(){
+  const data = await FetchfromDMTB('movie/now_playing')
+  const dataArray = data.results
+  // console.log(dataArray);
+
+  dataArray.forEach((movie) => {
+    const div = document.createElement('div')
+    div.classList.add('swiper-slide')
+    div.innerHTML = `<a href="movie-details.html?id=${movie.id}">
+    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+  </a>
+  <h4 class="swiper-rating">
+    <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(1)}
+  </h4>
+    `
+    document.querySelector('.swiper-wrapper').appendChild(div)
+  })
+  initSwiper();
+}
+
+function initSwiper() {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
+}
+
 
 
 async function FetchfromDMTB(endpoint) {
@@ -194,9 +238,11 @@ function highlight() {
 function init() {
   switch (global.currentPage) {
     case '/':
+      DisplaySlider();
       DisplayPopMovies();
       break;
     case '/index.html':
+      DisplaySlider();
       DisplayPopMovies();
       break;
     case '/shows.html':
